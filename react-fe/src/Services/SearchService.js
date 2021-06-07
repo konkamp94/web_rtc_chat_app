b3cceabde7c4761b711d5aaa70e5fbb3300411bc
Konstantinos <konkamp94@gmail.com>
2021-06-07 15:23:13 +0300

    development urls

--- react-fe/src/Services/SearchService.js
+++ react-fe/src/Services/SearchService.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 
 
 // development
-// const api_url = 'http://localhost:9090'
+const api_url = 'http://localhost:9090'
 
 //  production
-const api_url = 'http://ec2-18-219-127-149.us-east-2.compute.amazonaws.com'
+// const api_url = 'http://ec2-18-219-127-149.us-east-2.compute.amazonaws.com'
 export default class SearchService extends Component{
     searchByUsername = (username) => {
         return axios.get(`${api_url}/user?username=${username}`)
